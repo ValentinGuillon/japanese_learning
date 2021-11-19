f56c6a520adae0fdd8d3e5cf1aecbbfaf5098567
Valentin_G <valentinkr93430@gmail.com>
2021-11-19 14:04:53 +0100

    adding complex kana traduction (romaji->kana) + double consonants trad'

--- romaji_to_kana_vcom.py
+++ romaji_to_kana_vcom.py
@@ -25,12 +25,15 @@ def romaji_to_hiragana(text):
 
     to_hiragana = ""
     c = "" 
-    
+    text = " " + text #c'est pour les cas où la première lettre et la dernière sont deux mêmes consonnes. Parceque dans la suite, le prog fait des vérif' sur le i-1, et si c'est la première lettre, ça va regarder la dernière lettre.
     i = 0
     for x in text:
         j = 0
-        if(x == "n"): #if is "n"
-            if(len(text[i:]) == 1): #if is "n" on the last character
+        #if is "n"
+        if(x == "n"): 
+            #if is "n" on the last character
+            if(len(text[i:]) == 1):
+                j = 0
                 for y in char['romaji']['all']:
                     if(y == x):
                         c = char['hiragana']['all'][j]
@@ -38,9 +41,11 @@ def romaji_to_hiragana(text):
                     j += 1
                     
                 to_hiragana += c
-                #print(c) 
+                #print(c)
             
-            elif(not text[i+1] in vowels and not text[i+1] in vowels_circumflex): #if is not "n + vowel"
+            #if is "n + not vowel"
+            elif(not text[i+1] in vowels and not text[i+1] in vowels_circumflex):
+                j = 0
                 for y in char['romaji']['all']:
                     if(y == x):
                         c = char['hiragana']['all'][j]
@@ -50,51 +55,59 @@ def romaji_to_hiragana(text):
                 to_hiragana += c
                 #print(c)
 
-        elif(x in vowels_circumflex): #if is "vowel_circumflex"
+        #if is "vowel_circumflex"
+        elif(x in vowels_circumflex): 
             if(x == "â"):
-                temp_c = "a"
+                x = "a"
             elif(x == "ê"):
-                temp_c = "e"
+                x = "e"
             elif(x == "î"):
-                temp_c = "i"
+                x = "i"
             elif(x == "ô"):
-                temp_c = "o"
+                x = "o"
             elif(x == "û"):
-                temp_c = "u"
+                x = "u"
 
-            if(not text[i-1] in consonnes): #if is not "cons + vowel_circumflex"
+            #if is "vowel_circumflex" solo
+            if(not text[i-1] in consonnes):
+                j = 0
                 for y in char['romaji']['all']:
-                    if(y == temp_c):
-                        c = char['hiragana']['all'][j] + char['hiragana']['all'][j]
+                    if(y == x):
+                        c = char['hiragana']['all'][j]
                         break
                     j += 1
-                    
                 to_hiragana += c
                 #print(c)
-                
-            elif(text[i-1] in consonnes): #if is "cons + vowel_circumflex"
-                if(text[i-2] in consonnes and not text[i-2] == "n"): #if is "cons + cons + vowel_circumflex"
-                    c = text[i-2] + text[i-1] + temp_c
+            
+            #if is "cons + vowel_circumflex"
+            elif(text[i-1] in consonnes):
+                #if is "cons + cons + vowel_circumflex" and consonnes are same
+                if(text[i-2] == text[i-1]):
+                    c = text[i-1] + x
+                    j = 0
                     for y in char['romaji']['all']:
                         if(y == c):
                             c = char['hiragana']['all'][j]
                             break
                         j += 1
-                    to_hiragana += c
+                    to_hiragana += small_tsu + c
                     #print(c)
- 
+
+                #if is "cons1 + cons2 + vowel_circumflex" and cons1 is not "n"
+                elif(text[i-2] in consonnes and not text[i-2] == "n"):
+                    c = text[i-2] + text[i-1] + x
                     j = 0
                     for y in char['romaji']['all']:
-                        if(y == temp_c):
+                        if(y == c):
                             c = char['hiragana']['all'][j]
                             break
                         j += 1
-                    
                     to_hiragana += c
                     #print(c)
 
                 else:
-                    c = text[i-1] + temp_c
+                    c = text[i-1] + x
+                    j = 0
                     for y in char['romaji']['all']:
                         if(y == c):
                             c = char['hiragana']['all'][j]
@@ -103,30 +116,47 @@ def romaji_to_hiragana(text):
                     to_hiragana += c
                     #print(c)
 
-                    j = 0
-                    for y in char['romaji']['all']:
-                        if(y == temp_c):
-                            c = char['hiragana']['all'][j]
-                            break
-                        j += 1
-                    to_hiragana += c
-                    #print(c)
+            j = 0
+            for y in char['romaji']['all']:
+                if(y == x):
+                    c = char['hiragana']['all'][j]
+                    break
+                j += 1
+            to_hiragana += c
 
 
-        elif(x in vowels): #if is "vowel"
-            if(not text[i-1] in consonnes): #if is not "cons + vowel"
+        #if is "vowel"
+        elif(x in vowels): 
+            #if is not "cons + vowel"
+            if(not text[i-1] in consonnes):
+                j = 0
                 for y in char['romaji']['all']:
                     if(y == x):
                         c = char['hiragana']['all'][j]
                         break
                     j += 1
-                    
                 to_hiragana += c
                 #print(c)
-                
-            elif(text[i-1] in consonnes): #if is "cons + vowel"
-                if(text[i-2] in consonnes and not text[i-2] == "n"): #if is "cons + cons + vowel"
+
+            #if is "cons + vowel"    
+            elif(text[i-1] in consonnes):
+                #if is "cons + cons + vowel" and consonnes are same
+                if(text[i-2] in consonnes and text[i-2] == text[i-1]): 
+                    c = text[i-1] + x
+                    j = 0
+                    for y in char['romaji']['all']:
+                        if(y == c):
+                            c = char['hiragana']['all'][j]
+                            break
+                        j += 1
+                    
+                    to_hiragana += small_tsu + c
+                    #print(c)
+
+                #if is "cons + cons + vowel" and cons1 is not "n"
+                elif(text[i-2] in consonnes and not text[i-2] == "n"): 
                     c = text[i-2] + text[i-1] + x
+                    j = 0
                     for y in char['romaji']['all']:
                         if(y == c):
                             c = char['hiragana']['all'][j]
@@ -137,6 +167,7 @@ def romaji_to_hiragana(text):
                     #print(c)
                 else:
                     c = text[i-1] + x
+                    j = 0
                     for y in char['romaji']['all']:
                         if(y == c):
                             c = char['hiragana']['all'][j]
@@ -144,12 +175,14 @@ def romaji_to_hiragana(text):
                         j += 1
                     to_hiragana += c
                     #print(c)
-                    
-        elif(not x in all): #if is space or not a letter (!&@...)
+
+        #if is space or not a letter (!&@...)            
+        elif(not x in all): 
             to_hiragana += x
             #print(x)
         i += 1
     
+    to_hiragana = to_hiragana[1:] #et mtn on enlève l'espace du début
     return(to_hiragana)
 
 
