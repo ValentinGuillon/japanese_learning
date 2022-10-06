d829d2cca7fdb81dd9121a12117070f7f643d4bb
Valentin_G <valentin.guillon93430@gmail.com>
2022-10-06 14:00:10 +0200

    progress in 'module_list_jap_fr' modification

--- module_list_jap_fr.py
+++ module_list_jap_fr.py
@@ -28,36 +28,71 @@ expressions = 1
 
 class W(NamedTuple) :
     kana: chr #'h' or 'k' pour "hira" et "kata"
+    jap: str
     fr: str
-    jap: int
 
-def printW(a):
-    print(f" {a.fr} -> {a.jap}")
+def printW(word):
+    print(f" {word.jap} -> {word.fr}")
 
 
 
 #mots jap
 all_jap = []
-transports_jap = [W("Kuruma", W("Sukûtâ", W("Jitensha", W("Takushî", W("Chikatetsu", W("Densha", W("Hikôki", W("Fune", W("Baiku", W("Basu"]
-colors_jap = [W("Shiro", W("Chairo", W("Kiiro", W("Haiiro", W("Ao", W("Kuro", W("Aka", W("Orenji", W("Murasaki", W("Midori", W("Pinku"]
-animals_jap = [W("Inu", W("Ahiru", W("Usagi", W("Saru", W("Nezumi", W("Uma", W("Kuma", W("Tora", W("Raion", W("Niwatori", W("Shika", W("Buta", W("Neko", W("Tori", W("Kitsune", W("Ushi", W("Zô", W("Hitsuji", W("Koara", W("Panda" ]
-weather_jap = [W("Kaminari", W("Kumori", W("Hare", W("Samui", W("Yuki", W("Atsui", W("Ame"]
-clothes_jap = [W("Kutsushita", W("Kutsu", W("Jîpan", W("Sukâto", W("Kôto", W("Zubon", W("Tîshatsu", W("Sandaru", W("Tan pan", W("Doresu", W("Mizugi" ]
-food_jap = [W("Tamanegi", W("Budô", W("Kamoniku", W("Orenji", W("Suika", W("Tômorokoshi", W("Remon", W("Burokkorî", W("Kyûri", W("Ichigo", W("Ninjin", W("Kokonattsu", W("Jagaimo", W("Bêkon", W("Gyûnyû", W("Kyabetsu", W("Kinoko", W("Kabocha", W("Mizu", W("Tomato", W("Ocha", W("Eiyô dorinku", W("Jûsu", W("Kohitsuji", W("Ringo", W("Banana", W("Kôra", W("Kôhî", W("Sakana", W("Gyûniku", W("Toriniku", W("Nashi", W("Butaniku", W("Painappuru"]
-house_jap = [W("Eakon", W("Kamera", W("Karendâ", W("Kurejittokâdo", W("Konpyûta", W("Sutereo / Sutereofonikku", W("Supûn", W("Sofâ", W("Têburu", W("Terebi / Terebijon", W("Toire / Toiretto", W("Doa", W("Naifu", W("Nôto", W("Fôku", W("Beddo", W("Pasokon", W("Pen / Bôrupen", W("Rajio", W("Ranpu", W("Rimokon"]
+transports = [W('h', "Kuruma", "Voiture"), W('k', "Sukûtâ", "Scooter"), W('h', "Jitensha", "Vélo"), W('k', "Takushî", "Taxi"), W('h', "Chikatetsu", "Métro"), W('h', "Densha", "Train"), W('h', "Hikôki", "Avion"), W('h', "Fune", "Bateau"), W('k', "Baiku", "Moto"), W('k', "Basu", "Bus")]
+'''
+colors = [W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , )]
+colors_jap = ["Shiro", "Chairo", "Kiiro", "Haiiro", "Ao", "Kuro", "Aka", "Orenji", "Murasaki", "Midori", "Pinku"]
+animals = [W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , )]
+animals_jap = ["Inu", "Ahiru", "Usagi", "Saru", "Nezumi", "Uma", "Kuma", "Tora", "Raion", "Niwatori", "Shika", "Buta", "Neko", "Tori", "Kitsune", "Ushi", "Zô", "Hitsuji", "Koara", "Panda" ]
+weather = [W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , )]
+weather_jap = ["Kaminari", "Kumori", "Hare", "Samui", "Yuki", "Atsui", "Ame"]
+clothes = [W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , )]
+clothes_jap = ["Kutsushita", "Kutsu", "Jîpan", "Sukâto", "Kôto", "Zubon", "Tîshatsu", "Sandaru", "Tan pan", "Doresu", "Mizugi" ]
+food = [, W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , )]
+food_jap = ["Tamanegi", "Budô", "Kamoniku", "Orenji", "Suika", "Tômorokoshi", "Remon", "Burokkorî", "Kyûri", "Ichigo", "Ninjin", "Kokonattsu", "Jagaimo", "Bêkon", "Gyûnyû", "Kyabetsu", "Kinoko", "Kabocha", "Mizu", "Tomato", "Ocha", "Eiyô dorinku", "Jûsu", "Kohitsuji", "Ringo", "Banana", "Kôra", "Kôhî", "Sakana", "Gyûniku", "Toriniku", "Nashi", "Butaniku", "Painappuru"]
+house = [W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , )]
+house_jap = ["Eakon", "Kamera", "Karendâ", "Kurejittokâdo", "Konpyûta", "Sutereo / Sutereofonikku", "Supûn", "Sofâ", "Têburu", "Terebi / Terebijon", "Toire / Toiretto", "Doa", "Naifu", "Nôto", "Fôku", "Beddo", "Pasokon", "Pen / Bôrupen", "Rajio", "Ranpu", "Rimokon"]
+divers = [W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , )]
 divers_jap = ["Ai", "Ue", "Aoi", "Ie", "Iie", "Sekai", "Kagi", "Akai", "Osushi", "Kugi", "Keigo", "Saka", "Ya", "Hanaya", "Sakanaya", "Yuki", "Yoru", "Karada", "Rikai", "Ji", "Omatsuri", "Kuuki", "Tokei", "Kinoe", "Sakka", "Irasutoneitaa", "Shashinka", "Seiyuu", "Eegakantoku", "Keesan", "Kodomo no sensee", "Gaka", "Kyaku", "Jugyoe", "Ocha", "Ryokoe", "Sanmyaku"]
+verbs = [W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , )]
 verbs_jap = ["Hanasu", "Yomu", "Asobu", "Morau", "Matsu", "Omou", "Motsu", "Nomu", "Kiku", "Shinu", "Nugu", "Iu", "Kau", "Sumu", "Iku", "Noru", "Naru", "Wakaru", "Aru", "Oshieru", "Miru", "Taberu", "Iru", "Dekiru", "Kaeru", "Yameru", "Ageru", "Kuru", "Suru"]
+adjs = [W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , )]
 adjs_jap = ["Hayai", "Tanoshii", "Samui", "Sugoi", "Ii", "Kakkoii", "Warui", "Atsui", "Chiisai", "Yasui", "Omoshiroi", "Muzukashii", "Itai", "Takai", "Kowai", "Yasashii", "Atarashii", "Isogashii", "Ookii", "Oishii", "Jôzu", "Kirei", "Suki", "Kirai", "Kantan", "Hen", "Taihen", "Anzen", "Genki", "Benri"]
+conjug_verbs = [W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , )]
 conjug_verbs_jap = ["-u -> -imasu", "-u -> -imasen", "-u -> -imashita", "-u -> -imasen deshita",
                         "-u -> -tta", "-ru -> -tta", "-tsu -> -tta", "-gu -> -ida", "-bu -> -nda", "-mu -> -nda", "-nu -> -nda", "-su -> -shita", "-ku -> -ita",
                         "-u -> -wanai", "-_u -> -_anai", "-tsu -> -tanai",
                         "-u -> -wanakatta", "-_u -> -_anakatta", "-tsu -> -tanakatta",
                     "-ru -> -masu", "-ru -> -masen", "-ru -> -mashita", "-ru -> -masen deshita", "-ru -> -ta", "-ru -> -nai", "-ru -> -nakatta",
                     "Suru -> Shimasu  || Kuru -> Kimasu", "Suru -> Shimasen  || Kuru -> Kimasen", "Suru -> Shimashita  || Kuru -> Kimashita", "Suru -> Shimasen deshita  || Kuru -> Kimasen deshita", "Suru -> Shita  || Kuru -> Kita", "Suru -> Shinai  || Kuru -> Kinai", "Suru -> Shinakatta  || Kuru -> Kinakatta"]
+conjug_adjs = [W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , ), W('0', , )]
 conjug_adjs_jap = ["-i -> -kunai", "-i -> -katta", "-i -> -kunakatta", "-i -> -sô", "-i -> -kunasasô", "-i -> -ku",
                   "+ janai", "+ datta", "+ janakatta", "+ -sô", "+ -janasasô", "+ ni"]
+expressions = [W('0', , )]
 expressions_jap = ["Sumimasen" ]
+'''
 
+
+
+'''
+for n in transports:
+    printW(n)
+'''
+'''
+print("Hiragana :")
+for n in transports:
+    if n.kana == 'h':
+        printW(n)
+
+print("\nHiragana :")
+for n in transports:
+    if n.kana == 'k':
+        printW(n)
+'''
+
+
+
+'''
 #all_jap =
 for n in transports_jap:
     all_jap.append(n)
@@ -79,11 +114,11 @@ for n in verbs_jap:
     all_jap.append(n)
 for n in adjs_jap:
     all_jap.append(n)
-
+'''
 
 #mots fr
 all_fr = []
-transports_fr = ["Voiture", "Scooter", "Vélo", "Taxi", "Métro", "Train", "Avion", "Bateau", "Moto", "Bus"]
+
 colors_fr = ["Blanc", "Marron", "Jaune", "Gris", "Bleu", "Noir", "Rouge", "Orange", "Violet", "Vert", "Rose"]
 animals_fr = ["Chien", "Canard", "Lapin", "Singe", "Souris", "Cheval", "Ours", "Tigre", "Lion", "Poulet", "Biche", "Cochon", "Chat", "Oiseau", "Renard", "Vache", "Éléphant", "Mouton", "Koala", "Panda" ]
 weather_fr = ["Orageux", "Nuageux", "Ensoleillé", "Froid", "Enneigé", "Chaud", "Pluvieux"]
@@ -103,6 +138,8 @@ conjug_adjs_fr = ["Forme négative (adjectifs en i)", "Forme passée (adjectifs 
                   "Forme négative (adjectifs en na)", "Forme passée (adjectifs en na)", "Forme négative passée (adjectifs en na)", "Forme ''ça a l'air'' (adjectifs en na)", "Forme négative de ''ça a l'air'' (adjectifs en na)", "Transformation en adverbe (adjectifs en na)"]
 expressions_fr = ["Excusez-moi / Pardon pour le dérangement" ]
 
+
+'''
 #all_fr =
 for n in transports_fr:
     all_fr.append(n)
@@ -124,7 +161,7 @@ for n in verbs_fr:
     all_fr.append(n)
 for n in adjs_fr:
     all_fr.append(n)
-
+'''
 
 """
 #pour vérifie que les mots correspondent
